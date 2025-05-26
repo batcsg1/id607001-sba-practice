2b176f12be799cc0ef5bac6d317196e61124b957
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-26 18:04:02 +1200

    feat: added schemas to user validation

--- middleware/validation/user.js
+++ middleware/validation/user.js
@@ -2,6 +2,28 @@ import Joi from 'joi';
 
 const validatePostUser = (req, res, next) => {
   // Write your schema solution here
+  const postSchema = Joi.object({
+    firstName: Joi.string().min(3).max(100).required().messages({
+      "string.base": "firstName should be a string",
+      "string.empty": "firstName cannot be empty",
+      "string.min": "firstName should have a minimum length of {#limit}",
+      "string.max": "firstName should have a maximum length of {#limit}",
+      "any.required": "firstName is required"
+    }),
+    lastName: Joi.string().min(3).max(100).required().messages({
+      "string.base": "lastName should be a string",
+      "string.empty": "lastName cannot be empty",
+      "string.min": "lastName should have a minimum length of {#limit}",
+      "string.max": "lastName should have a maximum length of {#limit}",
+      "any.required": "lastName is required"
+    }),
+    email: Joi.string().pattern(new RegExp('^([a-z]+.[a-z]+)@outlook.com')).required().messages({
+      "string.base": "email should be a string",
+      "string.empty": "email cannot be empty",
+      "any.required": "email is required",
+      "string.pattern.base": "email must end with @outlook.com"
+    }),
+  });
 
   const { error } = postSchema.validate(req.body);
 
@@ -16,6 +38,25 @@ const validatePostUser = (req, res, next) => {
 
 const validatePutUser = (req, res, next) => {
   // Write your schema solution here
+  const putSchema = Joi.object({
+    firstName: Joi.string().min(3).max(100).optional().messages({
+      "string.base": "firstName should be a string",
+      "string.empty": "firstName cannot be empty",
+      "string.min": "firstName should have a minimum length of {#limit}",
+      "string.max": "firstName should have a maximum length of {#limit}",
+    }),
+    lastName: Joi.string().min(3).max(100).optional().messages({
+      "string.base": "lastName should be a string",
+      "string.empty": "lastName cannot be empty",
+      "string.min": "lastName should have a minimum length of {#limit}",
+      "string.max": "lastName should have a maximum length of {#limit}"
+    }),
+    email: Joi.string().pattern(new RegExp('^([a-z]+.[a-z]+)@outlook.com')).optional().messages({
+      "string.base": "email should be a string",
+      "string.empty": "email cannot be empty",
+      "string.pattern.base": "email must end with @outlook.com"
+    }),
+  });
 
   const { error } = putSchema.validate(req.body);
 
